3903bb86393b19af5be38fd0f86a4d3eff736b85
Atanda1 <45256233+Atanda1@users.noreply.github.com>
2019-08-18 07:35:49 +0100

    Updates

---
+++ precache-manifest.caa46f7d4b18b4200dcdda5b19678832.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1513d58af58f602002c33d240ac173a4",
+    "url": "/roboproject/index.html"
+  },
+  {
+    "revision": "b71c35cba9e2f5e588ec",
+    "url": "/roboproject/static/css/main.0f653632.chunk.css"
+  },
+  {
+    "revision": "1f0a964bf54b0eb7d6a3",
+    "url": "/roboproject/static/js/2.0df85ff2.chunk.js"
+  },
+  {
+    "revision": "b71c35cba9e2f5e588ec",
+    "url": "/roboproject/static/js/main.341c63f8.chunk.js"
+  },
+  {
+    "revision": "1e7d0e64e034f6d42891",
+    "url": "/roboproject/static/js/runtime~main.fcb9eb4e.js"
+  }
+]);
